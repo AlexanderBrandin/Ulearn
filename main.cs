d5c50ec18975ced6fc67980396818909d54828e6
Alexander Brandin <113559605+AlexanderBrandin@users.noreply.github.com>
2022-09-15 03:09:55 +0000

    Add cast to Hello, world!

--- main.cs
+++ main.cs
@@ -3,5 +3,12 @@ using System;
 class Program {
   public static void Main (string[] args) {
     Console.WriteLine ("Hello World");
+    double pi = Math.PI;
+    int tenThousand = 10000;
+    float tenThousandPi = (float) pi*tenThousand;
+    int roundedTenThousandPi = (int) Math.Round(tenThousandPi);
+    int integerPartOfTenThousandPi = (int) tenThousandPi;
+    Console.WriteLine(integerPartOfTenThousandPi);
+    Console.WriteLine(roundedTenThousandPi);
   }
 }
